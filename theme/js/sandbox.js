d3d023a01afdcbd892c3b4c286ab9ee3da819d8e
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2018-05-16 13:49:26 +0200

    Speed Kit docs for image optimization: sandbox in IFrame.

--- theme/js/sandbox.js
+++ theme/js/sandbox.js
@@ -1,8 +1,71 @@
-var options, url;
+var options, url, oldURL;
 var emptyOptions = '?bqoptimize=1;';
+var sizeRaw;
 
 function refreshOptimizedImage(providedOptions) {
+  updateParameters(providedOptions);
+  getImageInfo(true);
+
+  document.getElementById("options").value = options;
+  document.getElementById("image").src = url + options;
+}
+
+function getType(fastlyHeader, isOptimized) {
+  if (typeof fastlyHeader !== "string") {
+    return " ";
+  }
+
+  var value = fastlyHeader.match(new RegExp("(" + (isOptimized ? "ofmt" : "ifmt") + "=)([^\\s]+)"));
+  return value[2];
+}
+
+function getSize(fastlyHeader, isOptimized) {
+  if (typeof fastlyHeader !== "string") {
+    return " ";
+  }
+
+  var value = fastlyHeader.match(new RegExp("(" + (isOptimized ? "ofsz" : "ifsz") + "=)([^\\s]+)"));
+
+  var size = Number(value[2]) / 1000;
+  if (isOptimized) {
+    if (sizeRaw >0) {
+      var saving = ("" + (100 * ((sizeRaw - size) / sizeRaw))).replace(/\..*/, "");
+      return size + "KB" + (sizeRaw>size? " (" + saving + "% saved)" : "");
+    } else {
+      return size + "KB";
+    }
+  } else {
+    getImageInfo(true);
+    sizeRaw = size;
+    return size + "KB";
+  }
+}
+
+function getDimensions(fastlyHeader, isOptimized) {
+  console.log(fastlyHeader);
+
+  if (typeof fastlyHeader !== "string") {
+    return " ";
+  }
+  var value = fastlyHeader.match(new RegExp("(" + (isOptimized ? "odim" : "idim") + "=)([^\\s]+)"));
+  return value[2];
+}
+
+function getImageInfo(isOptimized) {
+  fetch(isOptimized ? url + options : url + "?bqoptimize=1", {method: 'HEAD'}).then((response) => {
+    var versionName = isOptimized ? "optimized" : "raw";
+    document.getElementById("imageInfo-content-type-" + versionName).innerHTML = getType(response.headers.get("fastly-io-info"), isOptimized);
+    document.getElementById("imageInfo-content-length-" + versionName).innerHTML = getSize(response.headers.get("fastly-io-info"), isOptimized);
+    document.getElementById("imageInfo-fastly-io-info-" + versionName).innerHTML = getDimensions(response.headers.get("fastly-io-info"), isOptimized);
+  });
+}
+
+function updateParameters(providedOptions) {
   url = document.getElementById("url").value;
+  if (url !== oldURL) {
+    oldURL = url;
+    getImageInfo(false);
+  }
   options = document.getElementById("options").value;
   if (typeof providedOptions === "object") {
     if (!options || options.length === 0) {
@@ -34,15 +97,12 @@ function refreshOptimizedImage(providedOptions) {
     options = "";
   }
 
-  document.getElementById("options").value = options;
-  document.getElementById("image").src = url + options;
-
   if (url && url.toLowerCase().indexOf('app.baqend.com') < 0) {
     document.getElementById("warning").classList.add("image-optimization-warning-visible");
   } else {
     document.getElementById("warning").classList.remove("image-optimization-warning-visible");
   }
-};
+}
 
 function refreshOptimizedImageDelayed() {
   debounce(refreshOptimizedImage, 100)();
@@ -64,5 +124,5 @@ function debounce(func, wait, immediate) {
 }
 
 
-document.getElementById("url").value = "https://ksm.app.baqend.com/v1/file/www/%2Bimg/flyingq-hd-opt.png";
+document.getElementById("url").value = "https://io-sandbox.app.baqend.com/baqend.png";
 refreshOptimizedImage({"width": 800, "height": undefined});
