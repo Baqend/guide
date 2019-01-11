b77b50889e63fd6303bffdae20531138de93cd33
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2019-01-11 11:04:43 +0100

    Build theme with Webpack, remove require.js

--- theme/js/modules/highlighting.js
+++ theme/js/modules/highlighting.js
@@ -1,6 +1,12 @@
-require([
-  base_url + '/js/lib/highlight.pack.js',
-], function (hljs) {
-  // Init Highlight JS
-  hljs.initHighlightingOnLoad()
-})
+import hljs from 'highlight.js/lib/highlight'
+import javascript from 'highlight.js/lib/languages/javascript'
+import css from 'highlight.js/lib/languages/css'
+import xml from 'highlight.js/lib/languages/xml'
+
+// Register languages we use
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('css', css)
+hljs.registerLanguage('xml', xml)
+
+// Initialize highlight.js
+hljs.initHighlightingOnLoad()
